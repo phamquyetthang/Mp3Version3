a52de777ef2577ebdb9b9b9d58bb52926766c857
phamquyetthang <quyetthang.phucchi@gmail.com>
2020-12-03 18:15:09 +0700

    upgrade ui modal playing music

--- src/containers/Playmusic/index.js
+++ src/containers/Playmusic/index.js
@@ -1,9 +1,10 @@
 import React, {useEffect, useState} from 'react';
 import {
+  Animated,
+  Easing,
   Image,
   Modal,
   StyleSheet,
-  Text,
   TouchableOpacity,
   View,
 } from 'react-native';
@@ -12,28 +13,34 @@ import TrackPlayer from 'react-native-track-player';
 import Progress from './Progress';
 import {useDispatch} from 'react-redux';
 import {unitH, unitW} from '../../asset/styles/size';
-import { setIsPlayingAction } from '../../redux/actions';
+import {setIsPlayingAction} from '../../redux/actions';
+import {
+  ContainerView,
+  PlayingBar,
+  Text1,
+  Text2,
+  TextTheme,
+} from '../../asset/styles/themes';
 export default function Playmusic({
   modalVisible,
   setModalVisible,
   song,
   setSong,
-  allMusic
+  allMusic,
 }) {
   const [isPlay, setIsPlay] = useState(true);
-  const [nextMusic,setNextMusic]=useState({})
+  const [nextMusic, setNextMusic] = useState({});
   const dispatch = useDispatch();
   useEffect(() => {
-    
-      for(let i=0;i<allMusic.length;i++){
-        if(song.id===allMusic[i].id){
-            setNextMusic(allMusic[i+1])
-        }
+    for (let i = 0; i < allMusic.length; i++) {
+      if (song.id === allMusic[i].id) {
+        setNextMusic(allMusic[i + 1]);
       }
-    
+    }
+
     console.log(song);
-    TrackPlayer.stop()
-    setIsPlay(true)
+    TrackPlayer.stop();
+    setIsPlay(true);
     TrackPlayer.setupPlayer().then(async () => {
       await TrackPlayer.add({
         id: 'trackId',
@@ -55,12 +62,26 @@ export default function Playmusic({
       setIsPlay(false);
     }
   };
+  const spinValue = new Animated.Value(0);
+  // First set up animation
+  Animated.loop(
+    Animated.timing(spinValue, {
+      toValue: 1,
+      duration: 10000,
+      easing: Easing.linear, // Easing is an additional import from react-native
+      useNativeDriver: true, // To make use of native driver for performance
+    }),
+  ).start();
+
+  // Next, interpolate beginning and end values (in this case 0 and 1)
+  const spin = spinValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '360deg'],
+  });
   return (
     <View>
-      <TouchableOpacity
-        onPress={() => setModalVisible(true)}
-        style={{backgroundColor: '#2A1B39'}}>
-        <Progress time={song.time} isPlay={0}/>
+      <PlayingBar onPress={() => setModalVisible(true)}>
+        <Progress time={song.time} isPlay={0} />
         <View
           style={{
             flexDirection: 'row',
@@ -68,8 +89,10 @@ export default function Playmusic({
           }}>
           <Image source={{uri: song.image}} style={styles.imagesongbottom} />
           <View>
-            <Text style={{fontSize: 18, color: '#fff'}}>{song.name}</Text>
-            <Text style={{color: '#fff'}}>{song.singer}</Text>
+            <TextTheme style={{fontSize: 18, fontWeight: 'bold'}}>
+              {song.name}
+            </TextTheme>
+            <TextTheme>{song.singer}</TextTheme>
           </View>
           <View
             style={{
@@ -78,62 +101,89 @@ export default function Playmusic({
               right: 8 * unitW,
               position: 'absolute',
             }}>
-            <Icon
-              name={'md-heart-outline'}
-              color="#fff"
-              size={22}
-              onPress={() => setModalVisible(false)}
-            />
-            <Icon
-              name={isPlay ? 'md-pause' : 'md-play'}
-              color="#fff"
-              size={22}
-              onPress={playmussic}
-              style={{marginHorizontal: 20 * unitW}}
-            />
-            <Icon
-              name={'md-play-skip-forward'}
-              color="#fff"
-              size={22}
-              onPress={() => dispatch(setIsPlayingAction(nextMusic))}
-            />
+            <TextTheme>
+              <Icon
+                name={'md-heart-outline'}
+                size={22}
+                onPress={() => setModalVisible(false)}
+              />
+            </TextTheme>
+            <TextTheme style={{marginHorizontal: 20 * unitW}}>
+              <Icon
+                name={isPlay ? 'md-pause' : 'md-play'}
+                size={22}
+                onPress={playmussic}
+              />
+            </TextTheme>
+            <TextTheme>
+              <Icon
+                name={'md-play-skip-forward'}
+                size={22}
+                onPress={() => dispatch(setIsPlayingAction(nextMusic))}
+              />
+            </TextTheme>
           </View>
         </View>
-      </TouchableOpacity>
+      </PlayingBar>
       <Modal animationType="slide" visible={modalVisible}>
-        <View
-          style={{alignItems: 'center', flex: 1, backgroundColor: 'hotpink'}}>
-          <TouchableOpacity>
-            <Icon name="md-chevron-down" />
-          </TouchableOpacity>
-          <View>
-            <Image source={{uri: song.image}} style={styles.imagesong} />
+        <ContainerView style={{alignItems: 'center'}}>
+          <View style={styles.hearderPopup}>
+            <TouchableOpacity onPress={() => setModalVisible(false)}>
+              <TextTheme style={styles.lable}>
+                <Icon name="md-chevron-down" size={32} />
+              </TextTheme>
+            </TouchableOpacity>
+            <View>
+              <Text1 style={styles.title}>{song.name}</Text1>
+              <Text2 style={styles.lable}>{song.singer}</Text2>
+            </View>
+            <TextTheme style={styles.options}>
+              <Icon name="md-ellipsis-vertical" size={24} />
+            </TextTheme>
+          </View>
+          <Animated.Image
+            style={[styles.imagesong, {transform: [{rotate: spin}]}]}
+            source={{uri: song.image}}
+          />
+          <View style={styles.slider}>
+            <Progress time={song.time} isPlay={0} />
           </View>
           {/* <Button title="ok" onPress={()=>setModalVisible(false)}/> */}
-          <Text style={styles.nameplay}>{song.name}</Text>
-          <Text style={styles.singerplay}>{song.singer}</Text>
+          <TextTheme style={styles.nameplay}>{song.name}</TextTheme>
+          <TextTheme style={styles.singerplay}>{song.singer}</TextTheme>
           <View style={styles.plpau}>
-            <Icon
-              name={'md-play-skip-back'}
-              color="#fff"
-              size={22}
-              onPress={() => setModalVisible(false)}
-            />
-            <Icon
-              name={isPlay ? 'md-pause' : 'md-play'}
-              color="#fff"
-              size={22}
-              onPress={playmussic}
-              style={{marginHorizontal: 60 * unitW}}
-            />
-            <Icon
-              name={'md-play-skip-forward'}
-              color="#fff"
-              size={22}
-              onPress={() => setModalVisible(false)}
-            />
+            <TextTheme>
+              <Icon name={'md-shuffle-outline'} size={28} />
+            </TextTheme>
+            <TextTheme>
+              <Icon
+                name={'md-play-skip-back'}
+                size={28}
+                onPress={() => setModalVisible(false)}
+              />
+            </TextTheme>
+            <TextTheme>
+              <Icon
+                name={
+                  isPlay ? 'md-pause-circle-outline' : 'md-play-circle-outline'
+                }
+                size={80}
+                onPress={playmussic}
+                style={{fontWeight: '100'}}
+              />
+            </TextTheme>
+            <TextTheme>
+              <Icon
+                name={'md-play-skip-forward'}
+                size={28}
+                onPress={() => setModalVisible(false)}
+              />
+            </TextTheme>
+            <TextTheme>
+              <Icon name={'md-repeat-outline'} size={28} />
+            </TextTheme>
           </View>
-        </View>
+        </ContainerView>
       </Modal>
     </View>
   );
@@ -146,16 +196,16 @@ const styles = StyleSheet.create({
     borderRadius: 4,
   },
   imagesong: {
-    width: 250 * unitW,
-    height: 250 * unitH,
+    width: 300 * unitW,
+    height: 300 * unitH,
     alignItems: 'center',
     marginTop: 60 * unitH,
-    borderRadius: 8,
+    borderRadius: 150 * unitH,
   },
   modalsong: {
     width: '100%',
     height: '100%',
-    backgroundColor: 'red',
+    // backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
     marginLeft: 20 * unitW,
@@ -172,7 +222,32 @@ const styles = StyleSheet.create({
   plpau: {
     flexDirection: 'row',
     paddingTop: 16 * unitH,
-    height: 60 * unitH,
-    // margintop:60
+    // height: 60 * unitH,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    marginHorizontal: 24 * unitW,
+    width: '100%',
+  },
+  hearderPopup: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    margin: 4 * unitW,
+    // backgroundColor: 'red',
+    width: '100%',
+  },
+  title: {
+    marginBottom: 6 * unitH,
+    marginLeft: 16 * unitW,
+  },
+  lable: {marginLeft: 16 * unitW},
+  options: {
+    position: 'absolute',
+    right: 4,
+  },
+  slider: {
+    marginTop: 40 * unitH,
+    alignItems: 'flex-start',
+    alignSelf: 'flex-start',
+    marginHorizontal: 24 * unitW,
   },
 });
