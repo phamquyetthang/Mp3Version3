f684b5a72baf7c18b704202cbb5ce6e73bfaff0c
phamquyetthang <quyetthang.phucchi@gmail.com>
2020-12-15 22:33:23 +0700

    final

--- src/containers/Router.js
+++ src/containers/Router.js
@@ -9,6 +9,7 @@ import Inplaylist from '../components/Inplaylist';
 import Stream from './Library/Stream';
 import Login from './Login';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {Button} from 'react-native';
 
 const Stack = createStackNavigator();
 export default function Router() {
@@ -18,25 +19,43 @@ export default function Router() {
     <ThemeProvider theme={theme}>
       <NavigationContainer>
         <Stack.Navigator
-          screenOptions={{
-            header: () => null,
-          }}>
-          <Stack.Screen name="Main" component={TabMenu} />
-          {hasAcc && <Stack.Screen name="Login" component={Login} />}
+        // screenOptions={{
+        //   header: () => null,
+        // }}
+        >
+          <Stack.Screen
+            name="Main"
+            component={TabMenu}
+            options={{animationTypeForReplace: 'pop', headerShown: false}}
+          />
+          {hasAcc && (
+            <Stack.Screen
+              name="Login"
+              component={Login}
+              options={{animationTypeForReplace: 'pop', headerShown: false}}
+            />
+          )}
           <Stack.Screen
             name="SearchForm"
             component={SearchForm}
-            options={{animationTypeForReplace: 'pop'}}
+            options={{animationTypeForReplace: 'pop', headerShown: false}}
           />
           <Stack.Screen
             name="Stream"
             component={Stream}
-            options={{animationTypeForReplace: 'pop'}}
+            options={{animationTypeForReplace: 'pop', headerShown: false}}
           />
           <Stack.Screen
             name="Inplaylist"
             component={Inplaylist}
-            options={{animationTypeForReplace: 'pop'}}
+            options={{
+              animationTypeForReplace: 'pop',
+              headerStyle: {
+                backgroundColor: '#0d021a',
+              },
+              headerTintColor: '#fff',
+              headerTitle: 'Playlist',
+            }}
           />
         </Stack.Navigator>
       </NavigationContainer>
